1373ce458f82ce0845201050d999d91ad15301cd
Jayamurugan Ravikumar <jayamurugancbe@gmail.com>
2023-12-01 14:20:47 +0100

    day1 part1

--- day1.py
+++ day1.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
+#Bring back the unaltered values
+
+input_file = open("input_day1.txt", "r")
+values_sum = 0
+
+for amended_values in input_file.readlines():
+    split_values = [*amended_values]
+    split_digits = [str(s) for s in split_values if s.isdigit()]
+    length = len(split_digits)
+    if length > 1:
+        old_value = int(split_digits[0]+split_digits[-1])
+    else:
+        old_value = int(split_digits[0]+split_digits[0])
+    values_sum += old_value 
+
+print(values_sum)
 
