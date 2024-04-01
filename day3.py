7578f302b9b861b611266862ca8f3295c5740a4b
Jay <Ravikumar@igmr.rwth-aachen.de>
2024-04-01 14:20:10 +0200

    clean

--- day3.py
+++ day3.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+import numpy as np
+
+sum_all = 0
+data = open("test_input.txt").read().strip()
+lines = data.split('\n')
+grid = [[m for m in l] for l in lines]
+print(grid)
+
+#for row in range(len(grid)):
+#    number = 0 
+#    for col in range(len(grid[row])):
+        
+
